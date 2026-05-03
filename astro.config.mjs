33dc79f48e7f5a666dfcd412f3e2da711d964b5c
raduandreigorcea <198594982+raduandreigorcea@users.noreply.github.com>
2026-05-03 15:51:24 +0300

    the latest deploy had broken the css

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-const isProductionBuild = process.env.NODE_ENV === 'production';
-
 export default defineConfig({
   site: 'https://raduandreigorcea.github.io',
-  base: isProductionBuild ? '/raduandreigorcea/' : '/',
+  base: '/',
 });
